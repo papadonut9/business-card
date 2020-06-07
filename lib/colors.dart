a3d6187cda85f8a4c04da09207723b861d140803
papadonut9 <anchit.m@somaiya.edu>
2020-06-08 04:12:11 +0530

    Stateless widget created

--- lib/colors.dart
+++ lib/colors.dart
@@ -2,3 +2,4 @@
 
 import 'package:flutter/material.dart';
 
+const MiCard_Scaffold0 = Colors.teal;
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
-
+import 'colors.dart';
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MiCard());
+}
+
+class MiCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: Text('Hello World'),
+        body: Text(
+          'Hello World',
+          textAlign: TextAlign.center,
+        ),
+        backgroundColor: MiCard_Scaffold0,
       ),
-    ),
-  );
+    );
+  }
 }
