ebe53f8ac97d3fbb604a0a2e8dcf145beaac94cb
papadonut9 <anchit.m@somaiya.edu>
2020-06-10 03:55:11 +0530

    Clean House

--- lib/containers.dart
+++ lib/containers.dart
@@ -1,18 +1,18 @@
 import 'dependencies.dart';
 
-Container containerNull(){
-  return Container(
-    height: 5,
-    // width: double.infinity,
-  );
-}
+// Container containerNull(){
+//   return Container(
+//     height: 5,
+//     // width: double.infinity,
+//   );
+// }
 
-SizedBox spacer() {
-  // creates a spacing between the two containers
-  return SizedBox(
-              height: 5,
-              width: 5,
-            );
-}
+// SizedBox spacer() {
+//   // creates a spacing between the two containers
+//   return SizedBox(
+//               height: 5,
+//               width: 5,
+//             );
+// }
 
 
--- lib/micard.dart
+++ lib/micard.dart
@@ -9,4 +9,3 @@ Scaffold miCard() {
     )
   );
 }
-
